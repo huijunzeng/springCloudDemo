d360e34a38e46ee52968b17441511ef0e5f9ad86
huijunzeng <18826233829@163.com>
2019-07-06 19:26:37 +0800

    dashboard以及turbine的整合

--- feign-consumer/src/main/java/com/ningmeng/consumer/FeignConsumerApplication.java
+++ feign-consumer/src/main/java/com/ningmeng/consumer/FeignConsumerApplication.java
@@ -1,10 +1,11 @@
 package com.ningmeng.consumer;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -21,5 +22,19 @@ public class FeignConsumerApplication {
     @Bean //可在这配置负载均衡策略  不配置默认为轮询策略，这里配置为随机策略，策略详细内容可查看README.md
     public IRule iRule() {
         return new RandomRule();
+    }
+
+    /**
+     * 相比springboot2.0以下版本无需配置默认直接输入/hystrix.stream，springboot2.0版本以上在结合hystrix的dashboard控制面板时，需要注入一个HystrixMetricsStreamServlet，并且设置访问路径/actuator/hystrix.stream或者/hystrix.stream，此外还需在配置文件设置开放/actuator端点
+     * @return
+     */
+    @Bean(name = "hystrixRegistrationBean")
+    public ServletRegistrationBean servletRegistrationBean() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
--- hystrix-turbine/src/main/java/com/ningmeng/turbine/HystrixTurbineApplication.java
+++ hystrix-turbine/src/main/java/com/ningmeng/turbine/HystrixTurbineApplication.java
@@ -1,18 +1,19 @@
-package com.ningmeng.dashboard;
+package com.ningmeng.turbine;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
-//@SpringCloudApplication  //相当于这三个注解@SpringBootApplication @EnableDiscoveryClient @EnableCircuitBreaker
-@SpringBootApplication
-@EnableDiscoveryClient
+@EnableTurbine // 开启turbine监控数据聚合
 @EnableHystrixDashboard // 开启hystrix仪表盘
-public class HystrixDashboardApplication {
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HystrixTurbineApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HystrixDashboardApplication.class, args);
+        SpringApplication.run(HystrixTurbineApplication.class, args);
     }
-
 }
