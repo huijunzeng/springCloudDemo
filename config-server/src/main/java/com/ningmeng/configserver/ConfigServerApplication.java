369bf885863ae824a9f367d97cf6951f38bcc620
huijunzeng <18826233829@163.com>
2019-07-09 01:38:07 +0800

    spring cloud config

--- config-server/src/main/java/com/ningmeng/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/ningmeng/configserver/ConfigServerApplication.java
@@ -1,5 +1,17 @@
 package com.ningmeng.configserver;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer //开启配置中心
+@EnableDiscoveryClient // 将config-server注册到配置的服务注册中心
 public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
